ada853814ed2d44ee9312b05e6e9f8ead58252cf
unknown <rakkakeren@gmail.com>
2023-02-19 00:47:24 +0700

    reformat code | add random number | add atk function

--- Game/simple_rpg.cpp
+++ Game/simple_rpg.cpp
@@ -1,14 +1,32 @@
+#include<algorithm>
+#include<ctime>
 #include<iostream>
 #include<iomanip>
+#include<random>
 #include<string>
+
+// RnG
+std::default_random_engine generator(std::time(0));
+std::uniform_real_distribution<double> distribution(0.0,1.0);
+
+// Constant
+float minDamage = 0.5;
 
 
 class Player {
     public: 
         std::string name;
-        float hp, damage, defense, critChance, critDamage;
+        int damage, defense;
+        float hp, critChance, critDamage;
 
-        Player(std::string name, int hp, int damage, int defense, float critChance, float critDamage) {
+        Player(
+            std::string name, 
+            float hp, 
+            int damage, 
+            int defense, 
+            float critChance, 
+            float critDamage
+        ) {
             this -> name = name;
             this -> hp = hp;
             this -> damage = damage;
@@ -16,19 +34,51 @@ class Player {
             this -> critChance = critChance;
             this -> critDamage = critDamage;
         }
+
+        float getCriticalDamage(int damage) {
+            double num = distribution(generator);
+            if (Player::critChance <= num) {
+                return damage * Player::critDamage;
+            }
+            return damage;
+        }
     
-        float attack(Player player) {
-            float totalDamage = damage - player.defense;
-            player.hp -= totalDamage;
+        float attack(Player& opponent) {
+            int damage = std::rand() % Player::damage + 1;                          // damage ranges from (1, player_damage)
+            float critDamage = getCriticalDamage(damage);                           // calculate crit damage
+            float totalDamage = std::max(minDamage, critDamage - opponent.defense); // deal minimal damage if defense exceed damage
+            opponent.hp -= totalDamage;
             return totalDamage;
         }
 };
 
+void attackAction(Player& subject, Player& target) {
+    float damage = subject.attack(target);
+    std::cout << subject.name << " hit " << target.name << " with " << damage << " damage." << std::endl;
+}
 
 
 int main() {
-    Player player1("main player", 100., 5., 2., 10., 1.5);
-    Player player2("opponent player", 100., 5., 2., 10., 1.5);
+    
+    std::srand(time(0));
+
+    Player player1(
+        "main player",      // name
+        100.,               // hp
+        5,                  // damage
+        2,                  // defense
+        0.1,                // crit chance
+        1.5                 // crit damage
+    );
+
+    Player player2(
+        "opponent player",  // name
+        100.,               // hp
+        5,                  // damage
+        2,                  // defense
+        0.1,                // crit chance
+        1.5                 // crit damage
+    );
 
     std::cout << "Welcome to simple RPG in C++, made by: Rakka." << std::endl
               << "It is a turn based game where every player has a chance to take an action" << std::endl;
@@ -37,14 +87,17 @@ int main() {
     float damage;
 
     while (player1.hp > 0 && player2.hp > 0) {
+         // HP status
+        std::cout << player1.name << " hp: " << player1.hp << std::endl
+                  << player2.name << " hp: " << player2.hp << std::endl;
+
         std::cout << "Select your action [a=attack, q=quit]: ";
         std::cin >> action;
 
         switch (action) {
             case 'a':
                 // Declaring var inside switch will cause error
-                damage = player1.attack(player2);
-                std::cout << player1.name << " hit " << player2.name << " with " << damage << " damage." << std::endl;
+                attackAction(player1, player2);
                 break;
             case 'q':
                 std::exit(3);
@@ -54,11 +107,7 @@ int main() {
         }
 
         // Player2 attack
-        damage = player2.attack(player1);
-        std::cout << player1.name << " hit " << player2.name << " with " << damage << " damage." << std::endl;
-
-        // HP status
-        std::cout << player1.name << " hp: " << player1.hp << std::endl
-                  << player2.name << " hp: " << player2.hp << std::endl;
+        attackAction(player2, player1);
+        std::cout << std::endl;
     }
 }
