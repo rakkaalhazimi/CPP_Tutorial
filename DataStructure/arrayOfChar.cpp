1d5f922505e5668c8ed35f7e028d4924dae9df7a
unknown <rakkakeren@gmail.com>
2023-05-10 23:38:31 +0700

    change: char to const

--- DataStructure/arrayOfChar.cpp
+++ DataStructure/arrayOfChar.cpp
@@ -7,7 +7,7 @@ int main(int argc, char const *argv[]) {
     std::cout << seq << std::endl;
 
     // point at first element
-    char *seqPtr = "abcde";
+    const char *seqPtr = "abcde";
     std::cout << *(seqPtr + 1) << std::endl; // get 2nd element of char
 
     return 0;
