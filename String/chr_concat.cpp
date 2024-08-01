ccfc6fc84418fa5e2c7e6a9276ec1db1f95f8bc4
Rakka Alhazimi <rakkakeren@gmail.com>
2024-08-01 21:27:01 +0700

    added: char concat with pointer

--- String/chr_concat.cpp
+++ String/chr_concat.cpp
@@ -1,15 +1,25 @@
 #include<iostream>
+#include<cstring>
 #include<string>
 
 
 int main(int argc, char const *argv[])
 {
-  
+  // Addition operator
   const char *name = "rakka";
   std::string firstName = name;
   std::string lastName = "alhazimi";
   
   std::cout << firstName + lastName;
+  
+  
+  // Pointer method
+  char *s1 = "rakka", *s2 = "alhazimi";
+  char *p = s1 + strlen(s1);
+  
+  strcpy(p, s2);
+  
+  std::cout << s1 << std::endl;
   
   
   return 0;
