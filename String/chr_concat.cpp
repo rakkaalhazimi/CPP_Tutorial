856a13fd04a748e00d9cca9cc05c9e49115744d3
Rakka Alhazimi <rakkakeren@gmail.com>
2024-08-01 21:51:14 +0700

    fixed: segmentation fault char concat

--- String/chr_concat.cpp
+++ String/chr_concat.cpp
@@ -10,17 +10,27 @@ int main(int argc, char const *argv[])
   std::string firstName = name;
   std::string lastName = "alhazimi";
   
-  std::cout << firstName + lastName;
-  
+  std::cout << firstName + lastName << std::endl;
   
   // Pointer method
-  char *s1 = "rakka", *s2 = "alhazimi";
-  char *p = s1 + strlen(s1);
+  const char *s1 = "rakka", *s2 = "alhazimi";
+  char *s1_copy = new char[strlen(s1) + strlen(s2) + 1]; // One for null terminating char '/0'
   
-  strcpy(p, s2);
+  // Don't do this
+  //
+  // char *s1 = "rakka";
+  // char *s2 = "alhazimi";
+  // char *p = s1 + strlen(s1);
+  // strcpy(p, s2);
+  //
+  // This will lead to segmentation fault, because we try to
+  // write the read-only memory, memory beyond s1, which will lead to
+  // undefined behavior.
   
-  std::cout << s1 << std::endl;
+  strcpy(s1_copy, s1);
+  strcat(s1_copy, s2);
   
-  
+  std::cout << s1_copy << std::endl;
+   
   return 0;
 }
