397fbb845665f612ea712964128c0df0ed650503
Rakka Alhazimi <rakkakeren@gmail.com>
2024-08-04 17:20:25 +0700

    added: Header folder

---
+++ Header/searching_header.cpp
@@ -0,0 +1,13 @@
+// If the name is enclosed by angle brackets, <....>
+// it will search the name in the include folder only.
+#include<iostream>
+
+// Else, it will search the name in your working directory.
+// Since we don't have iostream.h, this code will obviously throw error.
+#include "iostream.h"
+
+
+int main() {
+  
+  return 0;
+}
