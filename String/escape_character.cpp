0ac402a4aab2a3a94d61b0481e9a4019f3863235
Rakka Alhazimi <rakkakeren@gmail.com>
2025-03-01 18:54:34 +0700

    added: escape character

---
+++ String/escape_character.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+
+int main(int argc, char const *argv[])
+{
+  // \b escape character for backspace
+  // 'a' will shows 2 times instead of 3
+  std::cout << "rakka\baa" << std::endl;
+  
+  return 0;
+}
